38344954e6b3e2eccbb1e9e4f3db049c150eaab8
David Radchenko <davidradchenko2@gmail.com>
2023-03-29 19:31:13 -0700

    test

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* ========================= typing animation ==============*/
+var typed =new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Graphic Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+// switcher on navigeter web side
